3e3be85998e24bef7eb698ce91aad3e75cc2a7eb
andrew.rot <arotc7@gmail.com>
2016-11-24 14:17:08 -0500

    Fix a module not having a function

--- spawner.js
+++ spawner.js
@@ -2,17 +2,19 @@ var workerSpawner = require('workerSpawner');
 var attackSpawner = require('fighterSpawner');
 
 var spawner = {
-    for(var name in Memory.creeps) {
-        if(!Game.creeps[name]) {
-            delete Memory.creeps[name];
-            console.log('Clearing non-existing creep memory:', name);
+    spawn: function(spawn) {
+        for(var name in Memory.creeps) {
+            if(!Game.creeps[name]) {
+                delete Memory.creeps[name];
+                console.log('Clearing non-existing creep memory:', name);
+            }
         }
-    }
 
-    var spawners[wokerSpawner, attackSpawner];
+        var spawners[wokerSpawner, attackSpawner];
 
-    for(var spawner in spawners) {
-        spawner.spawn(Game.spawns["Spawn1"]);
+        for(var spawner in spawners) {
+            spawner.spawn(spawn);
+        }
     }
 }
 
