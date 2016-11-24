31c14a53d2a0566b40f54c1aba25ebb87219959c
andrew.rot <arotc7@gmail.com>
2016-11-24 10:13:44 -0500

    Update to include making roads over the paths that creeps move

--- build.road.js
+++ build.road.js
@@ -1,6 +1,6 @@
 var roadBuilder = {
     create: function(creep) {
-        if(creep.pos.look(LOOK_STRUCTURES).length < 1 && creep.pos.look(LOOK_CONSTRUCTION_SITES) < 1) {
+        if(creep.pos.look().length < 1) {
             creep.pos.createConstructionSite(STRUCTURE_ROAD);
         }
     }
--- main.js
+++ main.js
@@ -6,23 +6,24 @@ var wall = require('wall');
 var roadBuilder = require('build.road');
 
 module.exports.loop = function () {
-    
+
     spawner.spawn(Game.spawns["Spawn1"]);
-    
+
     // Define a wall inside of here to build
     //wall.build(Game.spawns["Spawn1"]);
-    
+
     for(var name in Game.creeps) {
         var creep = Game.creeps[name];
         if(creep.memory.role == 'harvester') {
             roleHarvester.run(creep);
-            //roadBuilder.create(creep);
+            roadBuilder.create(creep);
         }
         if(creep.memory.role == 'upgrader') {
             roleUpgrader.run(creep);
         }
         if(creep.memory.role == 'builder') {
             roleBuilder.run(creep);
+            roadBuilder.create(creep);
         }
     }
 }
