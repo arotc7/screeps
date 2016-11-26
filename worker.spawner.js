991e4d11d1009e6f4007d0e911649ae9b02d10da
andrew.rot <arotc7@gmail.com>
2016-11-26 09:50:44 -0500

    Update creating creeps bodies

--- worker.spawner.js
+++ worker.spawner.js
@@ -3,22 +3,24 @@ var constants = require('constants');
 var workerSpawner = {
 
     spawn: function(spawner) {
+        var creepType = [WORK, CARRY, MOVE];
+
+        cost = (constants.WORK_COST * 4) + constants.CARRY_COST + (constants.MOVE_COST * 2)
+        if(spawner.room.energyAvailable > cost) {
+            creepType = [WORK, WORK, WORK, WORK, CARRY, MOVE, MOVE];
+        }
+
         var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
         console.log('Harvesters: ' + harvesters.length);
 
         if(harvesters.length < constants.HARVESTERS_COUNT) {
-            var newName = spawner.createCreep([WORK,CARRY,MOVE], undefined, {role: 'harvester'});
+            var newName = spawner.createCreep(creepType, undefined, {role: 'harvester'});
             console.log('Spawning new harvester: ' + newName);
         }
 
         var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
         console.log('Upgraders: ' + upgraders.length);
         if(upgraders.length < constants.UPGRADERS_COUNT) {
-            var creepType = [WORK, CARRY, MOVE];
-
-            if(spawner.room.energyAvailable > 450) {
-                creepType = [WORK, WORK, WORK, WORK, CARRY, MOVE, MOVE];
-            }
 
             var newName = spawner.createCreep(creepType, undefined, {role: 'upgrader', upgrading: false});
             console.log('Spawning new upgrader: ' + newName);
@@ -27,7 +29,7 @@ var workerSpawner = {
         var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
         console.log('Builders: ' + builders.length);
         if(builders.length < constants.BUILDERS_COUNT) {
-            var newName = spawner.createCreep([WORK, CARRY, MOVE], undefined, {role: 'builder'});
+            var newName = spawner.createCreep(creepType, undefined, {role: 'builder'});
             console.log('Spawning new builder: ' + newName);
         }
     }
